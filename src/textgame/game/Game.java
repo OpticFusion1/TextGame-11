856ad5cc43cf81c65a3be98bef55f148da31dd0d
Tom Barrett <tbtommyb@gmail.com>
2017-02-01 13:28:23 +0000

    Added NPCs and test conversation (to be removed)

--- src/textgame/game/Game.java
+++ src/textgame/game/Game.java
@@ -1,6 +1,8 @@
 package textgame.game;
 
 import java.util.*;
+
+import textgame.conversation.ConversationManager;
 import textgame.entities.*;
 import textgame.items.*;
 import textgame.locations.*;
@@ -13,13 +15,19 @@ public class Game {
 	private Player player;
 	private State gameState;
 	private CommandParser commandParser;
-
+	private NPCs npcs;
+	
 	public Game(Locations locations, Player player) {
+		this(locations, player, null);
+	}
+
+	public Game(Locations locations, Player player, NPCs npcs) {
 		this.locations = locations.getAll();
 		this.player = player;
 		this.player.setLocation(locations.get(0));
 		setGameState(State.PLAYER_ALIVE);
 		this.commandParser = new CommandParser(this);
+		this.npcs = (npcs == null ? new NPCs() : npcs);
 	}
 	
 	public Player getPlayer() {
@@ -34,14 +42,22 @@ public class Game {
 		this.gameState = gameState;
 	}
 	
+	public NPCs getNPCs() {
+		return npcs;
+	}
+
+	public void setNPCs(NPCs npcs) {
+		this.npcs = npcs;
+	}
+
 	public void play() {
-		Scanner in = new Scanner(System.in);
+		ConversationManager convo = new ConversationManager(getNPCs().get("bouncer").getConversation());
+		convo.converse();
 		while(getGameState().equals(State.PLAYER_ALIVE)) {
 			Outputter.write("Enter command (exit, item, [q]uit, [i]nventory or [l]ocation): ");
-			String command = in.next();
+			String command = commandParser.getCommand();
 			commandParser.parse(command);
 		}
-		in.close();
 		Outputter.writeln("Exiting game");
 	}
 	
--- src/textgame/game/TextGame.java
+++ src/textgame/game/TextGame.java
@@ -1,15 +1,18 @@
 package textgame.game;
 
 import textgame.entities.*;
+import textgame.loaders.LoadResources;
 import textgame.locations.*;
 
 public final class TextGame {
 	public static void main(String[] args) {
 		try {
 			Locations locations = LoadResources.loadLocations("resources/Locations.xml");
+			NPCs npcs = LoadResources.loadNPCs("resources/NPCs.json");
+
 			Player player = new Player("tom");
 
-			Game game = new Game(locations, player);
+			Game game = new Game(locations, player, npcs);
 
 			game.play();
 		} catch (Exception e) {
